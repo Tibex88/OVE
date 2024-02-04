879688a3ddbb22b8db3d67689bf13587a6702e6b
Tibebe Solomon <87706650+Tibex88@users.noreply.github.com>
2024-02-04 20:55:15 +0300

    lock file

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,3 @@
-<script setup>
-import ActionPanel from './components/ActionPanel.vue';
-import VideoPlayer from './components/VideoPlayer.vue';
-
-  function getInstance(art) {
-      this.art = art;
-  }
-
-  let style={ width: "100%", height: "400px" }
-  let art= null
-
-</script>
-
 <template>
 
  <div class="full-grid">
@@ -26,16 +13,124 @@ import VideoPlayer from './components/VideoPlayer.vue';
     </div>
     <div id="item-2" class="bg-blueGray border">
       <ActionPanel />
-      2
+      <ScriptViewerVue v-model:subtitle="subtitle" />
     </div>
     <div id="resizer"></div>
     <div id="item-3" class="bg-blueGray border">
       <ActionPanel />
-      3
+      <EditorTableVue 
+        :art="art"
+        v-model:subtitle="subtitle"
+        :subtitle2="subtitle2" />
+      
     </div>
  </div>
 
 </template>
+
+<script setup>
+import ActionPanel from './components/actionpanel.vue';
+import VideoPlayer from './components/VideoPlayer.vue';
+import EditorTableVue from "./components/EditorTable.vue";
+import ScriptViewerVue from "./components/ScriptViewer.vue";
+
+import { fromVtt } from './assets/js';
+import { onMounted,computed, } from 'vue';
+import {useStore} from 'vuex'
+
+import srt from "./assets/srt"; //dev data
+import srt2 from "./assets/srt2"; //dev data
+import casts from "./assets/cast"; //dev data
+
+  // setup(props){
+const store =useStore();
+// let casts;
+// let srt;
+// let srt2;
+let style={ width: "100%", height: "400px" }
+let art= null
+let subObj = null;
+let srtParser;
+let vvtParser;
+let isDragging = false;
+// let style = { width = "100%", height: "400px" };
+let multiple= [
+  {
+    keyCode: 65,
+    modifiers: ["shiftKey"],
+    preventDefault: true,
+  },
+]
+  // };
+  // setup(){
+  // }
+ function  toSubtitle(srt) {
+      try {
+        let subtitle = fromVtt(srt);
+        return subtitle;
+      } catch (err) {
+        console.log(err);
+      }
+    }
+onMounted(() => {
+  // console.log(1)
+  // let subtitle = JSON.parse(window.localStorage.getItem("subtitle"));
+  // let subtitle2 = JSON.parse(localStorage.getItem("subtitle2"));
+  // let cast = JSON.parse(localStorage.getItem("cast"));
+  // let movie = JSON.parse(localStorage.getItem("movie"));
+
+  // if (subtitle && subtitle instanceof Object) {
+  //   console.log("is instance of object");
+  //   store.commit("saveSubtitle", JSON.stringify(subtitle));
+  // } else {
+    store.commit("saveSubtitle", toSubtitle(srt)); //def data
+  // }
+
+  // if (subtitle2 && subtitle2 instanceof Object) {
+  //   console.log("is instance of object");
+  //   store.commit("saveSubtitle2", subtitle2);
+  // } else {
+    store.commit("saveSubtitle2", toSubtitle(srt2)); //def data
+  // }
+
+  // if (cast && cast instanceof Array) {
+  //   console.log("is instance of array");
+  //   store.commit("saveCast", cast);
+  // } else {
+    store.commit("saveCast", casts); //def data
+  // }
+
+  // if (movie) {
+  //   console.log("is instance of string");
+  //   store.commit("saveMovie", movie);
+  // } else {
+    store.commit("saveMovie", "./src/assets/sample/1.mp4"); //def data
+  // }
+  })
+let movie = computed(() => {
+  return store.getters["movie"];
+})
+let cast = computed(() => {
+  return store.getters["cast"];
+})
+let subtitle2 = computed(() => {
+  return store.getters["cast"];
+})
+let subtitle = computed({
+  get(){
+        return store.getters["subtitle"];
+    },
+  set(value) {
+        if (value.upd == "subtitle") {
+          store.commit("updateSubtitle", value);
+        } else if (value.upd == "speaker") {
+          store.commit("updateSpeaker", value);
+        }
+      }
+})
+
+</script>
+
 
 <style scoped>
 
